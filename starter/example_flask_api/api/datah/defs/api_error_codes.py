7c6fd8b5cfbf7543783b70faa30927aff5ef293b
nate-udacity <mr.anderson317@gmail.com>
2021-08-31 16:53:20 -0400

    Add files via upload

---
+++ starter/example_flask_api/api/datah/defs/api_error_codes.py
@@ -0,0 +1,23 @@
+# Success, informative messages
+APIMSG_100 = "100: No Message."
+APIMSG_101 = "101: Created."
+APIMSG_102 = "102: Modified."
+APIMSG_103 = "103: Deleted."
+
+# API layout messages 600s
+APIMSG_600 = "600: Invalid endpoint."
+APIMSG_601 = "601: Method not allowed."
+
+# Input specific errors 700s
+APIMSG_700 = "700: Unable to parse JSON."
+APIMSG_701 = "701: At least one provided field is not modifiable."
+APIMSG_702 = "702: No matching record found."
+APIMSG_703 = "703: At least one field is unique and already exists."
+
+# API failures
+APIMSG_800 = "800: Delete Failed."
+
+# Authorization Failures
+APIMSG_900 = "900: Request Not Authorized."
+APIMSG_901 = "901: Authorization Failed."
+APIMSG_902 = "<dev> 902: Bearer Token Accepted."
