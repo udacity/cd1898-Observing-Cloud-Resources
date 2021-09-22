de7718747a70dc51aada98e2eb1ac8cb04a209fe
nate-udacity <mr.anderson317@gmail.com>
2021-09-22 18:03:31 -0400

    Add files via upload

---
+++ starter/terraform/modules/ec2/ec2.tf
@@ -0,0 +1,46 @@
+resource "aws_instance" "web" {
+  ami           = var.aws_ami
+  instance_type = "t3.micro"
+  key_name = "udacity"
+  tags = {
+    Name = "ubuntu"
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  vpc_id      = var.vpc_id
+
+  ingress {    
+    description = "web port"
+    from_port   = 80    
+    to_port     = 80
+    protocol    = "tcp"    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh port"
+    from_port   = 22    
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "monitoring"
+    from_port   = 9100    
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
+}
