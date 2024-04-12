67748d6fd7f4b51d405743aa958fc77789d2ac35
Eric Seidler <eric.seidler@gmail.com>
2024-04-12 12:01:48 -0500

    chg: update ami in tf

--- starter/terraform/ec2.tf
+++ starter/terraform/ec2.tf
@@ -7,11 +7,11 @@
 #    vpc_id             = module.vpc.vpc_id
 #  }
 
-  module "project_ec2" {
-   source             = "./modules/ec2"
-   name               = local.name
-   account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0ef8c7099ef697f3e"
-   private_subnet_ids = module.vpc.private_subnet_ids
-   vpc_id             = module.vpc.vpc_id
- }
+module "project_ec2" {
+  source             = "./modules/ec2"
+  name               = local.name
+  account            = data.aws_caller_identity.current.account_id
+  aws_ami            = "ami-0d61648afd8541e92"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+}
