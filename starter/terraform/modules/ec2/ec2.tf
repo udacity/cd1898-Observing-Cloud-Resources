646b98d114430cf201f3a91768acd85cf2e683f6
Islam Al-Afifi <31706646+islamalsawy@users.noreply.github.com>
2024-10-07 22:44:25 +0300

    Revert "Setting correct vpc and sg for standalone ec2 instance"

--- starter/terraform/modules/ec2/ec2.tf
+++ starter/terraform/modules/ec2/ec2.tf
@@ -1,57 +1,33 @@
 resource "aws_instance" "web" {
   ami           = var.aws_ami
   instance_type = "t3.micro"
-  key_name      = "udacity"
-  subnet_id     = data.aws_subnets.public.ids[0]
-
-  vpc_security_group_ids = [
-    aws_security_group.ec2_sg.id,
-    data.aws_security_group.default.id
-  ]
-
+  key_name = "udacity"
   tags = {
     Name = "ubuntu"
   }
 }
 
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-
-  filter {
-    name   = "tag:Name"
-    values = ["*public*"]
-  }
-}
-
-data "aws_security_group" "default" {
-  name   = "default"
-  vpc_id = var.vpc_id
-}
-
 resource "aws_security_group" "ec2_sg" {
-  name   = "ec2_sg"
-  vpc_id = var.vpc_id
+  name        = "ec2_sg"
+  vpc_id      = var.vpc_id
 
-  ingress {
+  ingress {    
     description = "web port"
-    from_port   = 80
+    from_port   = 80    
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "tcp"    
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "ssh port"
-    from_port   = 22
+    from_port   = 22    
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "monitoring"
-    from_port   = 9100
+    from_port   = 9100    
     to_port     = 9100
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
