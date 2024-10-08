af5656dbaee00d855ca1b16ac90fd018a30786b9
Islam Al-Afifi <31706646+islamalsawy@users.noreply.github.com>
2024-10-08 12:13:03 +0300

    Update _config.tf

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -2,7 +2,7 @@ terraform {
    backend "s3" {
      bucket = "udacity-terraform-course1"
      key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
+     region = "us-east-1"
    }
  }
 
