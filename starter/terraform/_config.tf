b30a0cc973ad622f3e1e6decd67f77514be40d2e
Sahil Goyal <sahilgoyal@Sahils-MacBook-Pro.local>
2024-12-23 18:23:26 +0530

    New Values and blackbox files provided

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,8 +1,8 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-course1"
+     bucket = "udacity-sre-terraform-testing" # Replace it with your S3 bucket name
      key    = "terraform/terraform.tfstate"
-     region = "us-east-1"
+     region = "us-east-2"  # Update to your desired region
    }
  }
 
